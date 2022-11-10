13bfe4b9ac3363a4236cb1b6dcd81bcb09eedf6b
Santacroce Flavio <santacroceflavio@citycable.ch>
2022-11-10 23:27:39 +0100

    update

---
+++ CV_Animated/script.js
@@ -0,0 +1,5 @@
+// TYPED
+var typed = new Typed('.typed', {
+    strings: ["Bonjour, je m’appelle Flavio Santacroce", "Après une longue carrière dans la restauration j’ai décidé de me reconvertir dans l’informatique. Cela fait maintenant 3 mois que j’ai commencé mon apprentissage d’informaticien à l’école des Arches à Lausanne. En plus des cours je me forme à coté en autodidacte aux divers langages informatiques comme HTML 5, CSS 3, Javascript, Python 3 et Mysql que j’utilise dans mes divers projets personnels et scolaire afin de réaliser mon objectif le plus rapidement possible, devenir développeur. Plus qu’un avenir professionnel je me suis découuvert une véritable vocation celle de coder."],
+    typeSpeed: 20,
+  });
